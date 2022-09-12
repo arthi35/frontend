a1ab81251a452440c3a0cdb3eeea8396a007f574
arthi35 <arthimba35@gmail.com>
2022-09-12 20:54:34 +0530

    bloguser

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -70,8 +70,8 @@ return data;
           <Button type='submit'variant='contained'sx={{borderRadius:3,marginTop:3}}color='warning'>Submit</Button>
           <Button onClick={()=>setIsSignup(!isSignup)} sx={{borderRadius:3,marginTop:3}}>
             Change To {isSignup?'Login':'Signup'}</Button>
-            <b>UserName:  hellouser@gmail.com</b>
-            <b>Password:  hellouser123</b>
+            <b style={{color:'blue',margin:'5px'}}>UserName:  hellouser@gmail.com</b>
+            <b style={{color:'blue',margin:'5px'}}>Password:  hellouser123</b>
       </Box>
     </form>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
     <div className="home">
       Hai 👋 Welcome To BlogApp If you Already have an account Please Login If You Are A New 
       User Please SignUp 😊
-      <p>UserName : hellouser@gmail.com</p>
-      <p>Password : hellouser123</p>
+      <p style={{margin:'15px'}}>UserName : hellouser@gmail.com</p>
+      <p style={{margin:'15px'}}>Password : hellouser123</p>
     </div>
   )
 }
